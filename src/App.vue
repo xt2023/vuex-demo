20f454f478f7f1374ab7a87781925384c1ed1828
xt2023 <2639695678@qq.com>
2022-05-29 22:39:10 +0800

    完成所有功能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="inputChange"/>
+    <a-button type="primary" @click="addItemList">添加事项</a-button>
+
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="cbChange($event,item.id)">{{ item.info }}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDone}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey==='all'?'primary':'default'" @click="btnChangeList('all')">全部</a-button>
+          <a-button :type="viewKey==='undone'?'primary':'default'" @click="btnChangeList('undone')">未完成</a-button>
+          <a-button :type="viewKey==='done'?'primary':'default'" @click="btnChangeList('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clearDone">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapState,mapGetters} from 'vuex'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+    //监听输入框改变事件
+    inputChange (e){
+      // console.log(e.target.value)
+      this.$store.commit('setInputValue',e.target.value)
+    },
+    //点击添加项
+    addItemList(){
+      if (this.inputValue.trim().length===0){return this.$message.warning('不能为空')}
+      this.$store.commit('addItem')
+    },
+    //点击移除item
+    removeById (id){
+      // console.log(id)
+      this.$store.commit('removeItem',id)
+    },
+    //复选框状态的改变
+    cbChange (e,id){
+      // console.log(e.target.checked)
+      // console.log(id)
+      const param={
+        id:id,
+        status:e.target.checked
+      }
+      this.$store.commit('changeStatus',param)
+    },
+    //清除已完成
+    clearDone (){
+      this.$store.commit('clearItem')
+    },
+    //改变数据列表
+    btnChangeList (key){
+      // console.log(key)
+      this.$store.commit('changeViewKey',key)
+    }
+  },
+
+  created() {
+    this.$store.dispatch('getList')
+
+  },
+  computed:{
+    ...mapState([,'inputValue','viewKey']),
+    ...mapGetters(['unDone','infoList'])
   }
 }
 </script>
 
-<style lang="less">
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,81 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list:[],
+    inputValue:'aaa',
+    id:5,
+    viewKey:'all'
   },
   getters: {
+    unDone (state){
+      return state.list.filter(x=>x.done==false).length
+    },
+    infoList(state){
+      if (state.viewKey==='all')return state.list
+      if (state.viewKey==='done') return state.list.filter(x=>x.done)
+      if (state.viewKey==='undone') return state.list.filter(x=>!x.done)
+      return state.list
+    }
   },
   mutations: {
+    initLIst(state,list){
+      state.list=list
+    },
+    setInputValue (state,value){
+      state.inputValue=value
+    },
+    //添加软件
+    addItem(state){
+      const obj={
+        id:state.id,
+        info:state.inputValue,
+        done:false
+      }
+      state.list.push(obj)
+      state.id++
+      state.inputValue=''
+    },
+    //碘酒移除item
+    removeItem(state,id){
+      const i=state.list.find(x=>{
+        x.id===id
+      })
+      if (i!==-1){
+        state.list.splice(id,1)
+      }
+    },
+    //复选框的改变
+    changeStatus (state,param){
+      const i=state.list.findIndex(x=>
+        x.id===param.id
+      )
+      if (i!==-1){
+        state.list[i].done=param.status
+      }
+    },
+    //清除已完成
+    clearItem (state){
+      state.list=state.list.filter(x=>x.done===false)
+    },
+    //修改视图关键字
+    changeViewKey (state,key){
+      state.viewKey=key
+    }
   },
   actions: {
+    getList(context){
+      axios.get('/list').then(({data})=>{
+        // console.log(data)
+        context.commit('initLIst',data)
+      })
+
+    },
+
   },
   modules: {
   }
